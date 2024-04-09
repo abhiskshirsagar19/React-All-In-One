53ce04bb0ca7b1859150906ed65b2705a98a274b
abhikshirsagar19 <abhikshirsagar1999@gmail.com>
2024-04-09 21:29:06 +0530

    JSX example added.

---
+++ Create_First_App/myfirstapp/src/example.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Example() {
+  return (
+    <div>
+      <h3>Hi Example !!!!</h3>
+      <p>Description!!</p>
+    </div>
+  );
+  //   return React.createElement(
+  //     "h3",
+  //     null,
+  //     "Hi Example !!!",
+  //     React.createElement("p", null, "Description!!!")
+  //   );
+}
---
+++ Create_First_App/myfirstapp/src/user.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function User() {
+  return (
+    <div>
+      <h2>Functional component created.</h2>
+    </div>
+  );
+}
+
+export default User;
+
+// export function User() {
+//   return (
+//     <div>
+//       <h2>Functional component created.</h2>
+//     </div>
+//   );
+// }
