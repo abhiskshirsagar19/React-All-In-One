35054b5ae3564855945acf131b7faa225341e47c
abhikshirsagar19 <abhikshirsagar1999@gmail.com>
2024-04-18 06:54:57 +0530

    New Folder Created React topic wise.

---
+++ React_Topic_Wise/react_topic_wise/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Democomponent from './Democomponent'
+function App() {
+  return (
+    <div className="App">
+     <Democomponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ React_Topic_Wise/react_topic_wise/src/Democomponent.js
@@ -0,0 +1,9 @@
+function democomponent() {
+  return (
+    <div>
+      <h1>Hi from component</h1>
+    </div>
+  );
+}
+
+export default democomponent;
