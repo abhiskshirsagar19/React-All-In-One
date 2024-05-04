39109b8cbb793a32a3dec05035b3ff614908e761
Abhi <abhikshirsagar1999@gmail.com>
2024-05-04 07:26:07 +0530

    Counnter Practice.

--- Create_First_App/myfirstapp/src/counter.jsx
+++ Create_First_App/myfirstapp/src/counter.jsx
@@ -1,8 +1,31 @@
-import { useState } from "react";
-import React from "react";
+// import { useState } from "react";
+// import React from "react";
 
-export default function Counter() {
+// export default function Counter() {
+//   let [count, setData] = useState(1);
+//   const increment = () => {
+//     setData(count + 1);
+//   };
+//   const decrement = () => {
+//     setData(count - 1);
+//   };
+//   return (
+//     <div>
+//       <p>Counter : {count}</p>
+//       <div>
+//         <button onClick={increment}>Increment</button>
+//       </div>
+//       <div>
+//         <button onClick={decrement}>Decrement</button>
+//       </div>
+//     </div>
+//   );
+// }
+import React, { useState } from "react";
+
+function Component() {
   let [count, setData] = useState(0);
+
   const increment = () => {
     setData(count + 1);
   };
@@ -21,3 +44,5 @@ export default function Counter() {
     </div>
   );
 }
+
+export default Component;
